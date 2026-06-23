559270b3c354b15c08a8b26347807e39eddddaa3
MaksimovGleb <maksimow.gleb@yandex.ru>
2026-06-23 18:37:30 +0300

    Request

---
+++ app/Http/Requests/StoreTaskRequest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreTaskRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'title' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'due_date' => 'nullable|date',
+            'create_date' => 'nullable|date',
+            'priority' => 'nullable|string|in:низкий,средний,высокий',
+            'category' => 'nullable|string|max:255',
+            'status' => 'nullable|string|in:выполнена,не выполнена',
+        ];
+    }
+}
