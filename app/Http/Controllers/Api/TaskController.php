b564f36f5def723eea99d8c8bd1aef7292f56545
MaksimovGleb <maksimow.gleb@yandex.ru>
2026-06-23 18:27:43 +0300

    controller

---
+++ app/Http/Controllers/Api/TaskController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreTaskRequest;
+use App\Http\Requests\UpdateTaskRequest;
+use App\Services\TaskService;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    public function __construct(
+        protected TaskService $taskService
+    ) {}
+
+    /**
+     * Отобразить список задач.
+     */
+    public function index(Request $request)
+    {
+        return $this->taskService->list($request);
+    }
+
+    /**
+     * Сохранить новую задачу в хранилище.
+     */
+    public function store(StoreTaskRequest $request): JsonResponse
+    {
+        $task = $this->taskService->create($request->validated());
+
+    }
+
+    /**
+     * Отобразить указанную задачу.
+     */
+    public function show(string $id): JsonResponse|array|\App\Models\Task
+    {
+        $task = $this->taskService->find($id);
+    }
+
+    /**
+     * Обновить указанную задачу в хранилище.
+     */
+    public function update(UpdateTaskRequest $request, string $id): JsonResponse
+    {
+        $task = $this->taskService->find($id);
+
+
+        $this->taskService->update($task, $request->validated());
+
+    }
+
+    /**
+     * Удалить указанную задачу из хранилища.
+     */
+    public function destroy(string $id): JsonResponse
+    {
+        $task = $this->taskService->find($id);
+
+        $this->taskService->delete($task);
+
+    }
+}
