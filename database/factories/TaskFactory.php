d70069ac3d1a389123290d8807bf328f82efa01c
MaksimovGleb <maksimow.gleb@yandex.ru>
2026-06-23 18:45:56 +0300

    factory, seeder

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Task;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Task>
+ */
+class TaskFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->sentence(3),
+            'description' => $this->faker->paragraph(),
+            'due_date' => $this->faker->dateTimeBetween('now', '+1 month'),
+            'create_date' => $this->faker->dateTimeBetween('-1 month', 'now'),
+            'status' => $this->faker->randomElement(['выполнена', 'не выполнена']),
+            'priority' => $this->faker->randomElement(['низкий', 'средний', 'высокий']),
+            'category' => $this->faker->randomElement(['Работа', 'Дом', 'Личное']),
+        ];
+    }
+}
--- database/migrations/2026_06_22_101245_create_tasks_table.php
+++ database/migrations/2026_06_22_101245_create_tasks_table.php
@@ -9,7 +9,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('tasks', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
             $table->string('title', 255);
             $table->text('description')->nullable();
             $table->dateTime('due_date')->nullable();
